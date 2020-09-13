98fc412054701d938aea873a3ff711bc4f0163ec
Gabriel Phiri <gabriel.phiri@bwb.mw>
2020-09-13 12:16:53 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -27,6 +27,11 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
+
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -35,11 +40,16 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        Intent dialIntent  = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(phoneUri);
+        startActivity(dialIntent);
+
+    }
 }
